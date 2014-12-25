e7f360833fdbea7dca4dbb86335d444d010d0352
pepelazz <pepelazz00@gmail.com>
2014-12-25 23:17:27 +0400

    1

---
+++ app.js
@@ -0,0 +1,2 @@
+(function(){var a;a=angular.module("app",[]),a.controller("main",["$rootScope","$scope","$http","$location","$timeout","$log",function(){}])}).call(this);
+//# sourceMappingURL=app.js.map
