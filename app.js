2b017a5b1c76c9fb4f7fbca51652e8f8047b4992
pepelazz <pepelazz00@gmail.com>
2014-12-30 12:46:49 +0400

    1

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
   module.controller('main', ['$rootScope', '$scope', '$http', '$location', '$timeout', '$log', (function($rootScope, $scope, $http, $location, $timeout, $log) {})]);
 
   module.controller('picturesGallery', [
-    '$rootScope', '$scope', '$http', '$location', '$timeout', '$log', (function($rootScope, $scope, $http, $location, $timeout, $log) {
-      var arrangePictures, imgLoad, mainAreaWidth;
+    '$rootScope', '$document', '$scope', '$http', '$location', '$timeout', '$log', (function($rootScope, $document, $scope, $http, $location, $timeout, $log) {
+      var arrangePictures, imgLoad, mainAreaWidth, pictureFullSize;
       mainAreaWidth = function() {
         return $('.main-area').width();
       };
@@ -47,6 +47,71 @@
       });
       $(window).resize(function() {
         return arrangePictures();
+      });
+      pictureFullSize = (function() {
+        var windowHeight, windowWidth;
+        windowWidth = $(window).width();
+        windowHeight = $(window).height();
+        $('.picture-wrapper').css('width', windowWidth).css("height", windowHeight);
+        $('.picture').css('max-width', windowWidth * 0.9).css('max-height', windowHeight * 0.9).css('width', 'auto').css("height", "auto");
+      });
+      $scope.galleryImg = {
+        currentImgFileName: null,
+        currentImgIndex: null,
+        show: (function(index) {
+          this.isBackdropShow = true;
+          this.currentImgFileName = picturesArray[index].fileName;
+          this.currentImgIndex = +index;
+          if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+            $scope.$apply();
+          }
+          pictureFullSize();
+        }),
+        close: (function() {
+          this.isBackdropShow = false;
+          this.currentImgFileName = null;
+          this.currentImgIndex = null;
+          if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+            $scope.$apply();
+          }
+        }),
+        isNextLeft: (function() {
+          return this.currentImgIndex !== 0;
+        }),
+        isNextRight: (function() {
+          return this.currentImgIndex !== picturesArray.length - 1;
+        }),
+        nextLeft: (function() {
+          this.currentImgIndex--;
+          this.currentImgFileName = picturesArray[this.currentImgIndex].fileName;
+          pictureFullSize();
+          if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+            $scope.$apply();
+          }
+        }),
+        nextRight: (function() {
+          this.currentImgIndex++;
+          this.currentImgFileName = picturesArray[this.currentImgIndex].fileName;
+          pictureFullSize();
+          if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+            $scope.$apply();
+          }
+        }),
+        isBackdropShow: false
+      };
+      $document.keydown(function(e) {
+        if ($scope.galleryImg.currentImgIndex !== null) {
+          if (e.keyCode === 37 && $scope.galleryImg.isNextLeft()) {
+            $scope.galleryImg.nextLeft();
+          }
+          if (e.keyCode === 39 && $scope.galleryImg.isNextRight()) {
+            $scope.galleryImg.nextRight();
+          }
+          if (e.keyCode === 27) {
+            $scope.galleryImg.close();
+          }
+        }
+        console.log(e.keyCode);
       });
     })
   ]);
